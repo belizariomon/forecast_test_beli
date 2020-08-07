b7937c746a5a55f417347bf4a9d5841ba6e78ab4
Belizario <belizariomon@users.noreply.github.com>
2020-08-07 20:52:33 -0300

    Fix azure.
    Faltaba el build

---
+++ client/build/precache-manifest.d711e69686ab5f0f4599eaf22784f832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60367c7b8e6357e2902cf16a09a57daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a8ee3b8e1fca3521c8",
+    "url": "/static/js/2.7b27e45e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b27e45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda264c67cb568c13f09",
+    "url": "/static/js/main.0481c577.chunk.js"
+  },
+  {
+    "revision": "45dfcb6d3000ee46bb72",
+    "url": "/static/js/runtime-main.bfa463e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
